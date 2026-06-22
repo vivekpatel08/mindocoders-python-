8f5409c40f06400dd732b480b236cd0b1eea33bf
vivekpatel08 <patelmrvivek@gamil.com>
2026-06-22 12:14:08 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,441 @@
+# print("Hello World!!")
+
+# #VIVEK
+# """hello world mindcoder"""
+# '''mindcoder'''
+
+# age = 19
+# print(type(age))
+
+# age = "Four"
+# print(type(age))
+
+# salary = 26500.56
+# print(type(salary))
+
+# name = "vivek"
+# profession = "AI Engineer"
+# experience = 3
+# print("Hello,I am",name,". I am a ",profession,"professionally. And I have around ",experience,"year of experience with it!")
+
+# #datatype
+# a = 3
+# print(a,type(a))
+# a = 3.0
+# print(a,type(a))
+# a = 3j
+# print(a,type(a))
+# a = ["apple","banna","mango"]
+# print(a,type(a))
+# a = ("apple","banana","grapse")
+# print(a,type(a))
+# a = range(6)
+# print(a,type(a))
+# a = {"name":"john","age":"19"}
+# print(a,type(a))
+# a = {"apple","banana","grapse"}
+# print(a,type(a))
+# a = frozenset({"apple","banana","grapse"})
+# print(a,type(a))
+# a = True
+# print(a,type(a))
+# a = b"Vivek"
+# print(a,type(a))
+# a = bytearray(5)
+# print(a,type(a))
+# a = memoryview(bytes(5))
+# print(a,type(a))
+# a = None
+# print(a,type(a))
+
+#operator
+'''arithmetic
+assignment
+comparison
+logical
+identity
+membership
+bitwise
+'''
+
+# print(" 10 + 2 = ",10 + 2)
+# print(" 10 - 2 = ",10 - 2)
+# print(" 10 * 2 = ",10 * 2)
+# print(" 10 / 2 = ",10 / 2)
+# print(" 10 % 2 = ",10 % 2)
+# print(" 10 // 3 = ",10 // 3)
+# print(" 10 ** 2 = ",10 ** 2)
+# print(5%-3)
+# print(5//-3)
+# print(5/-3)
+
+# assigment operators
+# a = 5
+# print(a)
+# a += 3
+# print(a)
+# a -= 2
+# print(a)
+# a *= 3
+# print(a)
+# a /= 2
+# print(a)
+# a //= 3
+# print(a)
+# a **= 2
+# print(a)
+# a = 5 
+# a%=3
+# print(a)
+# a |= 2
+# print(a)
+# a ^= 3
+# print(a)
+ 
+
+#camparison operator
+# a = 10
+# b = 20
+# print("a == b:",a == b)
+# print("a == 10:",a == 10)
+
+# print("a != b:",a != b)
+# print("a != 10:",a != 10)
+
+# print("a > b:",a > b)
+# print("a < 10:",a < 10)
+
+# print("a <= b:",a <= b)
+# print("a >= b:",a >= b)
+# print("a >= 10:",a >= 10)
+
+#logical operator
+# x = 10
+# print(x < 5 and x < 10)
+# print(x < 5 or x < 10)
+# print(not(x < 5 and x < 10))
+
+#identity operator
+
+# y = 3 
+# print(x is y)
+# print(x is not y)
+
+# X = ["maruti","BMW"]
+# Y =  ["maruti","BMW"]
+# Z=X
+
+# print(X is Y)
+# print(X is not Y)
+
+# print(Y is Z)
+# print(Y is not Z)
+
+# print(Z is X)
+# print(Z is not X)
+
+# X = ("maruti","BMW",)
+# Y =  ("maruti","BMW",)
+# Z=X
+
+# print(X is Y)
+# print(X is not Y)
+
+# print(Y is Z)
+# print(Y is not Z)
+
+# print(Z is X)
+# print(Z is not X)
+
+#membership operator
+
+# X = ["maruti","BMW"]
+# Y =  ["maruti","BMW"]
+
+
+# print("maruti" in X)
+# print("maruti" not in X)
+
+# print("maruti" in Y)
+# print("maruti" not in Y)
+
+'''bitwise operator'''
+# x =10
+# y = 20
+# print(x & y)
+# print(x | y)
+# print(x ^ y)
+# print(~x)
+# print(~y)
+# print( x << 2)
+# print( y << 2)
+# print( x >> 2)
+# print( y >> 2)
+
+
+'''python user input '''
+# name = input("please enter  your name:")
+# print("Ram Ram Sarayane😎",name)
+# age = input("please enter  your age:")
+# print("Ram Ram Sarayane😎",name , "you are",age,"years old")
+# phone = input("please enter  yor phone number:")
+# print("contact number:",phone )
+# email=input("enter email:")
+# print("email",email)
+
+# x = input("enter the first valuefor sum:")
+# y = input("enter the second valuefor sum:")
+# z = int(x) + int(y)   //type casting
+# print("sum :",z)
+
+# num1 =  int(input("enter the first number:"))
+# num2 = int(input("enter thr second number:"))
+# sum = num1 + num2
+# print("the sum is :",sum)
+
+# num1 = input("enter the first number:")
+# num2 = input("enter thr second number:")
+# sum = int(num1) + int(num2)
+# print("the sum is :",sum)
+
+
+# num1 =  input("enter the first number:")
+# num2 = input("enter thr second number:")
+# sum = float(num1) + float(num2)
+# print("the sum is :",sum)
+
+
+# num1 =  input("enter the first number:")
+# num2 = input("enter thr second number:")
+# sum = int(num1 +num2)
+# print("the sum is :",sum)
+
+'''write a program to calculate hypotense between to side''' 
+# p = 5
+# b = 5
+# h =(((p**2 )+(b**2))**(1/2))
+# print(h) 
+# 
+#  
+ #to print pattern using print function
+# print("+---------+")
+# print("|         |")
+# print("|         |")
+# print("|         |")
+# print("|         |")
+# print("|         |")
+# print("+---------+")
+
+# print("+" + "-"*10 + "+")
+# print(("|" + " "*10 +"|\n")*5,end="")
+# print("+" + "-"*10 + "+")
+
+'''important string'''
+# city = 'Bhopal'#string indexing 
+# print(city[0]) 
+# print(city[1]) 
+# print(city[2]) 
+# print(city[3]) 
+# print(city[4]) 
+# print(city[5]) 
+# print(city[-6])
+# print(city[-5]) 
+# print(city[-4]) 
+# print(city[-3]) 
+# print(city[-2])
+# print(city[-1])  
+# print(city[-1] + city[-4])
+
+# print(2 == 2)
+# print(2 == 2.0)
+
+# var = 0 
+# print(var == 0)
+
+# var = 0 
+# print(var == 1)
+
+'''control flow  decision making statement'''
+#var = 10
+# if var == 11 :
+#     print("mil gay yaar")
+# print("maja aa gya hoye!!!")
+# number1= int(input("enter the first value:"))
+# number2= int(input("enter the second value:"))
+# if number1 >number2:larger_number
+
+# else:larger_number
+
+# num1=int(input("enter first number:"))
+# num2=int(input("enter second number:"))
+# num3=int(input("enter third number:"))
+# larger_number=num1
+# if num2>larger_number:
+#     larger_number=num2
+# if num3>larger_number:
+#     larger_number=num3
+# larger_number=max(num1,num2,num3)
+# lowest_number=min(num1,num2,num3)
+# print("larger_number is :",larger_number)
+# print("lowest_number is :",lowest_number)
+
+# print("larger number is:",larger_number)
+
+
+#loop
+'''while'''
+# while True:     //infinte loop
+#     print("I am stuck in loop")
+
+# largest_number=-999999
+# number=int(input("enter a number or type -1 to stop:"))
+
+# while number!=-1:
+#     if number >largest_number:
+#         largest_number=number
+#     number=int(input("enter a number or type -1 to stop:"))
+# print("the largest_number is:",largest_number)
+
+'''pratice question baki hai abhi'''
+# print("Yes-Spathiphyllum is the best plant ever")
+
+# plant = input("Enter plant name:")   # keep input as string
+
+# if plant == "Spathiphyllum":
+#     print("Yes, Spathiphyllum is the best plant!")
+
+# elif plant == "spathiphyllum":
+#     print("No, I want a big Spathiphyllum!!")
+
+# else:
+#     print("Spathiphyllum! Not", plant)
+
+# total_amount= input("enter a annual thalers:")
+# if total_amount <= 85528:
+#     result = (total_amount * 0.18)-556
+# else:
+#     tax =  tax = 14839.02 + (total_amount - 85528) * 0.32
+# if tax <0:
+#     tax = 0
+# print("the tax is :",round(tax),"thalers")
+
+    
+
+
+
+# num = int(input("Please enter a number:"))
+# even_count  = 0 
+# odd_count = 0
+# while num != 0 :
+#     if num % 2 == 0 :
+#         even_count += 1
+#     else:
+#         odd_count += 1
+#     num = int(input("Please enter a number:"))
+# print("Even:",even_count)
+# print("odd:",odd_count)
+
+'''counter truty'''
+# counter = 5
+# while counter != 0 :
+#     print("Inside the loop.",counter)
+#     counter -=1
+# print("Outside the loop.",counter)
+
+# counter = 5 
+# while counter :
+#     print("Inside the loop.",counter)
+#     counter -=1
+# print("Outside the loop.",counter)
+
+''' for loop (range function)'''
+# for counter in range(100):
+#     print("Counter:",counter)
+
+# for counter in range(2,8):
+#     print("Counter:",counter)
+
+'''break'''
+# print("the break instruction:")
+# for counter in range(1,6):
+#     if counter == 3:
+#         break
+#     print("inside the loop.",counter)
+# print("outside the loop.")
+
+'''contiune'''
+# print("the break instruction:")
+# for counter in range(1,6):
+#     if counter == 3:
+#         continue
+#     print("inside the loop.",counter)
+# print("outside the loop.")
+
+# largest_number=-999999
+# counter=0
+# while True:
+#   number=int(input("enter a number or type -1 to stop:"))
+#   if number == -1: 
+#    break
+#    counter += 1
+#   if number > largest_number :
+#     largest_number=number
+# if counter != 0:
+#     print("the largest_number is:",largest_number)
+# else:
+#   print("invalid number!!")
+
+# largest_number=-999999
+# counter=0 
+# number=int(input("enter a number or type -1 to stop:"))
+# while number != -1:
+# #    if number == -1: 
+# #       continue
+#    counter += 1
+
+#    if number > largest_number :
+#     largest_number=number
+#    number=int(input("enter a number or type -1 to stop:"))
+   
+# if counter :
+#     print("the largest_number is:",largest_number)
+# else:
+#   print("you haven't entered any number.")
+# counter=1
+# while counter<5:
+#     print(counter)
+#     counter += 1
+# else:
+#     print("else:",counter)
+
+# counter=5
+# while counter<5:
+#     print(counter)
+#     counter += 1
+# else:
+#     print("else:",counter)
+
+
+
+
+
+
+
+
+
+
+
+
+'''list (samjh nahi aaya bhai kuch bhi)'''
+# numbers = [10,7,5,3,1]
+# numbers[0]=111
+# print("previous list content:",numbers)
+
+# numbers[1]=numbers[4]
+# print("previous list content:",numbers)
+
+# print("\nlist's length:",len(numbers))
+# del numbers[1]
+# print("New list's length:",len(numbers))
+
+# print("\nNew list content :",numbers)
