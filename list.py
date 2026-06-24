f184e3ec4edef6fdacf11028e2e357f07501fe11
vivekpatel <patelmrvivek@gmail.com>
2026-06-24 19:07:20 +0530

    list question

--- list.py
+++ list.py
@@ -6,13 +6,62 @@
 # print(f'length of list1:{len(list1)}')
 # #print("hello how are you ")
 
-list1 =[5,4,3,2,1]
-print(len(list1))
-print(list1)
-list1.append(4)
+# list1 =[5,4,3,2,1]
+# print(len(list1))
+# print(list1)
+# list1.append(4)
 
-print(len(list1))
-print(list1)
-list1.insert(0,222)
-print(len(list1))
-print(list1)
+# print(len(list1))
+# print(list1)
+# list1.insert(0,222)
+# print(len(list1))
+# print(list1)
+# l1 = [1,2,3,4,5]
+# # for i in l1:
+# #     print(i)
+# for count in range(len(l1)):
+#     print(l1[count])
+
+# my_list = [1,23,4,56,78]
+# for count in range(len(my_list)):
+#     print(my_list[count])
+
+
+# list1 = []
+# for count in range(1,11):
+#  list1.append(count)
+# print(list1)
+
+# my_list=[]
+# num = 1 
+# while num<=10:
+#     my_list.append(num)
+#     num+=1
+# print(my_list)
+
+# my_list = [10,20,30,40,50,60,70,80,90,100]
+# for i in range(10):
+#     my_list[i]+=1
+# print(my_list)
+
+# list1= []
+# for  i in range(10,101,10):
+#     list1.append(i+1)
+# print(list1)
+
+'''list ke sare element ka sum'''
+# my_list = [10,20,30,40,50,60,70,80,90,100]
+# sum = 0
+# for i in range(len(my_list)):
+#     sum +=my_list[i]
+# print("sum:",sum)
+
+'''swap two number'''
+a = 20
+b = 10
+print("a:",a)
+print("b:",b)
+#
+a,b=b,a
+print("a:",a)
+print("b:",b)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+l1 = [1,2,3,4,5]
+# # for i in l1:
+# #     print(i)
+# for count in range(len(l1)):
+#     print(l1[count])
